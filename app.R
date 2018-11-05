2d099cc1be53ac5ce4492db0d0c129325762d1ed
Saiyaz Kazi <skazi@college.harvard.edu>
2018-11-04 21:05:30 -0500

    commit w rough shiny app

---
+++ app.R
@@ -0,0 +1,37 @@
+library(shiny)
+library(tidyverse)
+
+# Define UI ----
+ui <- fluidPage(
+  titlePanel("Immigrant Voting Preferences by State"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Choose a State to See Immigrant Political Preferences."),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = v_temp,
+                  selected = "NY")
+      
+      ),
+    
+    mainPanel(
+      plotOutput("prefPlot")
+    )
+  )
+)
+# Define server logic ----
+server <- function(input, output) {
+  output$prefPlot <- renderPlot({
+    destination <- read_rds("use_shiny")
+    
+    destination %>%
+      group_by(V161010e) %>% 
+      ggplot(aes(x=V161064x)) + geom_bar()
+    
+  })
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
