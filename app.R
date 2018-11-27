77470f7895c7e5ef9edaf570480ba2d7eb5f24a6
Saiyaz Kazi <skazi@college.harvard.edu>
2018-11-27 11:15:02 -0500

    updated app

--- app.R
+++ app.R
@@ -7,6 +7,8 @@ library(readstata13)
 # Define UI ----
 ui <- fluidPage(
   titlePanel("Immigrant Voting Preferences by State"),
+  textAreaInput("caption", "Data Source", "This data comes from the 2016 ANES. 4000 respondents were asked to answer questions based on demographic, political, and social issues. The survey took place in two waves; right before and right after the 2016 general election.", width = "1000px"),
+  textAreaInput("caption", "GitHub Repo", "https://github.com/saiyazkazi/Final-Project---1005"),
   
   sidebarLayout(
     sidebarPanel(
@@ -27,11 +29,11 @@ ui <- fluidPage(
 # Define server logic ----
 server <- function(input, output) {
   output$prefPlot <- renderPlot({
-    destination <- read_rds("use_shiny")
+    destination <- read_rds(path = "use_shiny")
     
     destination %>%
       group_by(V161010e) %>% 
-      ggplot(aes(x=V161064x)) + geom_bar()
+      ggplot(aes(x=V161064x)) + geom_bar() + labs(x = "Party Vote")
     
   })
 }
